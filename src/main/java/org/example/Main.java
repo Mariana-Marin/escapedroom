c263fb61af14725222a6c0e47de9fdfdd2db4efa
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-06-03 10:55:48 -0500

    coo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Escaped Room");
+
+      package com.ejemplo;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+        public class Variables {
+
+            int edad;
+            double precioProducto;
+            int cantidadHijos;
+            double salario;
+            double altura;
+            boolean interruptorEncendido;
+            char genero;
+            String matriculaVehiculo;
+            int productosAlmacen;
+            double distanciaKm;
+            double temperatura;
+            String numeroTelefono;
+            LocalDate fechaNacimiento;
+            String codigoPostal;
+            int idProducto;
+            double puntajeExamen;
+            double velocidadAuto;
+            boolean mayorEdad;
+            boolean puertaAbierta;
+            long poblacionMundial;
+            char letraExamen;
+            String palabra;
+            int estrellasCalificacion;
+            boolean archivoActivo;
+            double precioBillete;
+            int puntuacionVideojuego;
+            boolean tieneLicencia;
+            double calificacionPromedio;
+            String nombreCompleto;
+            int cantidadLibros;
+            String codigoBarras;
+            int porcentajeBateria;
+            double tipoCambio;
+            String matriculaEstudiante;
+            double puntajePromedio;
+            char caracterEspecial;
+            double constantePlanck;
+            boolean sensorFuncionando;
+            LocalTime horaEvento;
+            boolean clienteRegistrado;
+            int idCliente;
+            double volumenRecipiente;
+            String nombreUsuario;
+            String codigoProducto;
+            int visitantesSitio;
+            boolean pagoConfirmado;
+            double peso;
+            double notaFinal;
+            char teclaPulsada;
+            String nombreEmpresa;
+
+        }
+
+
+
+
+    }
+}
