8ff71a0217634b3fc78624a171b9bb9df18a0bf2
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-06-03 11:09:01 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,13 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Escaped Room");
 
-      package com.ejemplo;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
         public class Variables {
 
